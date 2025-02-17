9e67514cd493a1b00068adddc4e2daefbc990fa7
bagsharu <gabs.haru@gmail.com>
2025-02-17 09:51:41 -0300

    Quick test of the fetchHTML method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            String url = "http://hiring.axreng.com/internship/example1.html";
+            System.out.println(HtmlProcess.fetchHTML(url));
+        } catch (IOException e) {
+            System.out.println("Error acessing the URL");
+        }
     }
 }
