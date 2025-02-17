ad6c1de3e7544b752d4f2a6a4ec06b09c5fbf8dc
bagsharu <gabs.haru@gmail.com>
2025-02-17 09:49:29 -0300

    Body of the method fetchHTML

--- src/HtmlProcess.java
+++ src/HtmlProcess.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 public class HtmlProcess {
@@ -11,5 +13,24 @@ public class HtmlProcess {
         // This object will be used to store the HTML structure
         StringBuilder contentHTML = new StringBuilder();
         URL url = new URL(urlRecieved);
+
+        // This block is used for fecthing the HTML
+
+        // With the usage of a BuffReader we efficiently read through the lines of the fectched data
+        // The "url.openStream()" connects to the url and returns data (bytes) from it, which is converted
+        // to text with the InputSream?Reader.
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(url.openStream()))) {
+
+            // This while block reades the HTML content line by line, then it adds to the context variable
+            // following the structure of a HTML block, by inputting the lines individually.
+            String inputLine;
+            while ((inputLine = input.readLine()) != null) {
+                contentHTML.append(inputLine).append("\n");
+            }
+        }
+
+        // Returns a String with the HTML
+        return contentHTML.toString();
+
     }
 }
