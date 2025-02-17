b6e6d8b6c30d219035ca8bb3a626c5a5b6ef4c22
bagsharu <gabs.haru@gmail.com>
2025-02-17 09:39:23 -0300

    Class variables for the method fetchHTML

--- src/HtmlProcess.java
+++ src/HtmlProcess.java
@@ -1,2 +1,15 @@
+import java.io.IOException;
+import java.net.URL;
+
 public class HtmlProcess {
+
+    // This class is responsible for manipulating the HTML from the user
+
+    // This method recieves an URL and
+    public static  String fetchHTML(String urlRecieved) throws IOException {
+
+        // This object will be used to store the HTML structure
+        StringBuilder contentHTML = new StringBuilder();
+        URL url = new URL(urlRecieved);
+    }
 }
